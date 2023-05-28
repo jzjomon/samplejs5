6e92a81bafa4d58dd376efb85a05b4ec9164d89d
jomon jose <jzjomon@gmail.com>
2023-05-28 20:00:34 +0530

    changecolor is finished

--- changeColor.js
+++ changeColor.js
@@ -1,3 +1,12 @@
-changecolor(){
-    let a = document.getElementById("colorselect").selectedindex;
-}console.log(a);
+function colorChange(){
+    let a = document.getElementById("colorSelect").value;
+    if(a=="Red"){
+        document.getElementById("text").style.color = "Red";
+    }else if(a=="Green"){
+        document.getElementById("text").style.color = "Green";
+    }else if(a=="White"){
+        document.getElementById("text").style.color = "White";
+    }else if(a=="Black"){
+        document.getElementById("text").style.color = "Black";
+    }
+}
